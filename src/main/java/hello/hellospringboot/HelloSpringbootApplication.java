82fcd1268ff995e57f982380d745b27de8af47f7
Jun-cloud <kjy8827@gmail.com>
2021-09-23 22:07:00 +0900

    Setting

--- src/main/java/hello/hellospringboot/HelloSpringbootApplication.java
+++ src/main/java/hello/hellospringboot/HelloSpringbootApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringbootApplication {
 
+	// start.spring.io 에서 기본 프로젝트 빌드
+	// Project : Gradle
+	// Dependencies : Spring Web, Thymeleaf
+	// GENERATE로 다운받은 후 project import
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringbootApplication.class, args);
 	}
