fcc78f3c0c1fb15fa81f1c9bd79dbb1904cf2dce
Jun-cloud <kjy8827@gmail.com>
2021-09-24 21:00:21 +0900

    Welcome page

--- src/main/java/hello/hellospringboot/HelloSpringbootApplication.java
+++ src/main/java/hello/hellospringboot/HelloSpringbootApplication.java
@@ -10,6 +10,9 @@ public class HelloSpringbootApplication {
 	// Project : Gradle
 	// Dependencies : Spring Web, Thymeleaf
 	// GENERATE로 다운받은 후 project import
+	
+	// 기본적으로 static/index.html 이 Welcome Page 기능을 함
+	// localhost:8080 -> static/index.html 이 열림
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringbootApplication.class, args);
 	}
