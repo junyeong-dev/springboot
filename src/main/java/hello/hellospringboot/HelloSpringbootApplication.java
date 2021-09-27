a1790783686fe771862473cd266e8e32343e20ea
Jun-cloud <kjy8827@gmail.com>
2021-09-27 20:45:05 +0900

    Build project

--- src/main/java/hello/hellospringboot/HelloSpringbootApplication.java
+++ src/main/java/hello/hellospringboot/HelloSpringbootApplication.java
@@ -10,7 +10,15 @@ public class HelloSpringbootApplication {
 	// Project : Gradle
 	// Dependencies : Spring Web, Thymeleaf
 	// GENERATE로 다운받은 후 project import
-	
+
+	// 빌드하기
+	// ./gradlew build
+	// 잘 안될경우
+	// ./gradlew clean build
+	// cd build/libs
+	// 실행하기
+	// java -jar hello-spring-0.0.1-SNAPSHOT.jar
+
 	// 기본적으로 static/index.html 이 Welcome Page 기능을 함
 	// localhost:8080 -> static/index.html 이 열림
 	public static void main(String[] args) {
