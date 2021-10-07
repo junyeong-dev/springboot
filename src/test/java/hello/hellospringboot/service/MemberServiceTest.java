464f6ee57278174e28041c1405e36809363ece97
junyeong-dev <kjy8827@gmail.com>
2021-10-07 20:56:46 +0900

    Create service test case part two

--- src/test/java/hello/hellospringboot/service/MemberServiceTest.java
+++ src/test/java/hello/hellospringboot/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import hello.hellospringboot.domain.Member;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
@@ -22,6 +23,31 @@ class MemberServiceTest {
         // then
         Member findMember =  memberService.findOne(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spting");
+
+        Member member2 = new Member();
+        member1.setName("spting");
+
+        // when
+        memberService.join(member1);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+//        try {
+//            memberService.join(member2);
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+        // then
 
     }
 
