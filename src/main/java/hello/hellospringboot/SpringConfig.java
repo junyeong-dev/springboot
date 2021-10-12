6607d07081a40210f98144787f5b222b7bdce6ba
junyeong-dev <kjy8827@gmail.com>
2021-10-12 21:06:29 +0900

    Spring Bean

---
+++ src/main/java/hello/hellospringboot/SpringConfig.java
@@ -0,0 +1,25 @@
+package hello.hellospringboot;
+
+import hello.hellospringboot.domain.Member;
+import hello.hellospringboot.repository.MemberRepository;
+import hello.hellospringboot.repository.MemoryMemberRepository;
+import hello.hellospringboot.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 자바 코드로 직접 스프링 빈 등록
+// 기본적으로 직접 등록하는 것이기 때문에 @Service, @Repository, @Autowired 어노테이션을 제거해야 한다.
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService((MemoryMemberRepository) memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+}
--- src/main/java/hello/hellospringboot/service/MemberService.java
+++ src/main/java/hello/hellospringboot/service/MemberService.java
@@ -9,13 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // Dependency Injection
-    @Autowired
     public MemberService(MemoryMemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
