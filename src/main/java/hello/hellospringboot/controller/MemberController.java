58e78ac6c6f43e707c378207f0047aa010d17b7c
junyeong-dev <kjy8827@gmail.com>
2021-10-15 19:49:28 +0900

    Create new member form

--- src/main/java/hello/hellospringboot/controller/MemberController.java
+++ src/main/java/hello/hellospringboot/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospringboot.controller;
 import hello.hellospringboot.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -30,6 +31,11 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/member/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 
 }
