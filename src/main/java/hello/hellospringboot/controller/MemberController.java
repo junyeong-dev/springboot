e22fde44e577f7d0ff47cab38a519c00140d92d2
junyeong-dev <kjy8827@gmail.com>
2021-10-18 20:36:54 +0900

    Create new member function

--- src/main/java/hello/hellospringboot/controller/MemberController.java
+++ src/main/java/hello/hellospringboot/controller/MemberController.java
@@ -1,9 +1,11 @@
 package hello.hellospringboot.controller;
 
+import hello.hellospringboot.domain.Member;
 import hello.hellospringboot.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -33,9 +35,19 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/member/new")
+    @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }
