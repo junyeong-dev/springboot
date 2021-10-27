ec32903b10d42f7403f6baf7f1da0149a0bd89dd
junyeong-dev <kjy8827@gmail.com>
2021-10-27 21:54:56 +0900

    Create Jdbc member save function

--- src/main/java/hello/hellospringboot/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospringboot/repository/JdbcMemberRepository.java
@@ -1,11 +1,11 @@
 package hello.hellospringboot.repository;
-
 import hello.hellospringboot.domain.Member;
-
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 public class JdbcMemberRepository implements MemberRepository {
 
     private final DataSource dataSource;
@@ -16,21 +16,72 @@ public class JdbcMemberRepository implements MemberRepository {
 
     @Override
     public Member save(Member member) {
-        return null;
-    }
+        String sql = "insert into member(name) values(?)";
 
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, member.getName());
+            pstmt.executeUpdate();
+            rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        return null;
+    }
+    @Override
+    public List<Member> findAll() {
+        return null;
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        return  null;
+    }
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
     }
 
-    @Override
-    public List<Member> findAll() {
-        return null;
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
     }
 }
